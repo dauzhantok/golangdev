626e23003e15c689151a9d10393f56f9f86ddd56
dauzhantok <43054395+dauzhantok@users.noreply.github.com>
2021-10-16 14:39:49 +0600

    hw5

--- main.go
+++ main.go
@@ -2,26 +2,80 @@ package main
 
 import (
 	"fmt"
-	"math"
-	"net/http"
+	"math/rand"
+	"sync"
 	"time"
 )
 
-type User struct {
-	Name           string
-	Kg, Hight, Imt float64
+func workerGenerator(worker int) <-chan int {
+	rand.Seed(time.Now().UnixNano())
+	ch := make(chan int)
+
+	go func() {
+		for i := 0; i < 5; i++ {
+			ch <- worker*1000 + i
+			time.Sleep(time.Duration(rand.Intn(1000)) * time.Millisecond)
+		}
+
+		close(ch)
+	}()
+
+	return ch
+}
+func merge(cs ...<-chan int) <-chan int {
+	ch := make(chan int)
+	wg := new(sync.WaitGroup)
+
+	for _, c := range cs {
+		wg.Add(1)
+
+		localC := c
+		go func() {
+			defer wg.Done()
+
+			for in := range localC {
+				ch <- in
+			}
+		}()
+	}
+	go func() {
+		wg.Wait()
+		close(ch)
+	}()
+
+	return ch
 }
 
-var da = User{"Dauzhan", 60, 1.8, 0}
+func worke(id int, bread <-chan int, result chan<- int) {
 
-func home(page http.ResponseWriter, r *http.Request) {
-	fmt.Fprintf(page, "%v have IMT %v", da.Name, da.Imt)
-	fmt.Fprintln(page, "")
-
+	for i := range bread {
+		fmt.Println("Worker id ", id, " make bread ", i)
+		time.Sleep(time.Second)
+		//fmt.Println("===========Second===========")
+		fmt.Println("Worker id ", id, " done bread ", i)
+		result <- i
+	}
 }
 func main() {
-	da.Imt = da.Kg / math.Pow(da.Hight, 2)
-	time.Sleep(1 * time.Second)
-	http.HandleFunc("/", home)
-	http.ListenAndServe(":9000", nil)
+	bread := make(chan int, 5)
+	result := make(chan int, 5)
+
+	for i := 0; i < 3; i++ {
+		go worke(i, bread, result)
+	}
+
+	for i := 0; i < 5; i++ {
+		bread <- i
+	}
+	close(bread)
+	time.Sleep(time.Second * 3)
+
+	mergeA := merge(workerGenerator(1), workerGenerator(2), workerGenerator(3))
+	for v := range mergeA {
+		fmt.Println(v)
+	}
+
+	for i := 0; i < 5; i++ {
+		fmt.Println("Bread cooked", <-result)
+	}
 }
